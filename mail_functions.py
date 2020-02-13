1e3dce0047438f5dc2beae1ca4bff9ca9f73ecc7
can kurt <can.kurt.aa@gmail.com>
2020-02-13 17:25:59 +0300

    logging added

--- mail_functions.py
+++ mail_functions.py
@@ -1,5 +1,20 @@
-
-def send_mail(sender, password, receivers, subject, body, body_html=None, Cc=[], Bcc=[], attachments=[], use_ssl=True, show_info=True, smtp_server_incoming="smtp.gmail.com"):
+def send_mail(sender, password, receivers, subject, body, body_html=None, Cc=[], Bcc=[], attachments=[], use_ssl=True, verbose=3, log_file="send_mail.log",smtp_server_incoming="smtp.gmail.com"):
+    """
+    sender: email address of the sender
+    password: senders email password
+    receivers: LIST of email addresses of receivers
+    subject: subject of the mail
+    body: mail body
+    body_html (None): html form of the mail body if exists (you still need to pass a text version of the body if client can't parse html version text version will be shown)
+    Cc ([]): LIST of emails of the Cc receivers
+    Bcc ([]): LIST of emails of the Bcc receivers
+    attachments ([]): LIST of attachment paths to include
+    use_ssl (True): uses tls if False
+    verbose (3): int value between 0-3 (log levels -> 3 info, 2 warning, 1 error, 0 critical) 
+    log_file ("send_mail.log"): log file name if None no file will be use
+    smtp_server_incoming ("smtp.gmail.com"): smtp server incoming address
+    """
+    # import libs
     import smtplib, ssl
     from email.mime.text import MIMEText
     from email.mime.multipart import MIMEMultipart
@@ -7,7 +22,34 @@ def send_mail(sender, password, receivers, subject, body, body_html=None, Cc=[],
     from email.mime.base import MIMEBase
     import datetime
     import os
+    import logging
 
+
+    # log stuff
+    logger = logging.getLogger(__name__)  
+    verbosity = {0:50,1:40,2:30,3:20}
+    if(verbosity.get(verbose)):
+        logger.setLevel(verbosity.get(verbose))
+    else:
+        logger.setLevel(20)
+    
+    # log formatter
+    formatter = logging.Formatter("%(asctime)s [%(levelname)s] -> %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+
+    # file handler
+    if(log_file):
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    # stream handler
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    
+
+
+    # set message stuff
     message = MIMEMultipart("alternative")
     message["Subject"] = subject
     message["From"] = sender
@@ -16,7 +58,7 @@ def send_mail(sender, password, receivers, subject, body, body_html=None, Cc=[],
         message["To"] = receiver
 
     # add cc to the message
-    # bcc are hidden
+    # bcc are hidden 
     if(Cc):
         for c in Cc:
             message["Cc"] = c
@@ -24,24 +66,21 @@ def send_mail(sender, password, receivers, subject, body, body_html=None, Cc=[],
     # add Cc and Bcc to receivers list
     receivers = receivers + Cc + Bcc
 
-    # Turn these into plain/html MIMEText objects
+    # parse body as plaintext
     plain = MIMEText(body, "plain")
     message.attach(plain)
     
-    # Add HTML/plain-text parts to MIMEMultipart message
-    # The email client will try to render the last part first
+    # parse body_html as html, email client will try to render the last part first
     if(body_html):
         html = MIMEText(body_html, "html")
         message.attach(html)
 
-
+    # attachments stuff
     if(attachments):
         for attachment in attachments:
             filename = os.path.basename(attachment)
             try:
                 with open(attachment, "rb") as attachment:
-                    # Add file as application/octet-stream
-                    # Email client can usually download this automatically as attachment
                     part = MIMEBase("application", "octet-stream")
                     part.set_payload(attachment.read())
 
@@ -55,28 +94,31 @@ def send_mail(sender, password, receivers, subject, body, body_html=None, Cc=[],
                 message.attach(part)
 
             except (OSError, IOError) as e:
-                print(e,"attachment error")
+                logger.warning("error can't include attachment {0} for more info see {1}".format(attachment,log_file), exc_info=True)
 
-
+    # convert message to string
     message = message.as_string()
 
-    if(use_ssl):
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server_incoming, 465, context=context) as server:
-            server.login(sender, password)
-            server.sendmail(sender, receivers, message)
-    else:
-        connection = smtplib.SMTP(smtp_server_incoming, 587)
-        connection.ehlo()
-        connection.starttls()
-        connection.ehlo()
-        connection.login(sender, password)
-        connection.sendmail(sender, receivers, message)
-        connection.close()
-    
-    if(show_info):
-        print("The mail has been sent. Time:{0}".format(str(datetime.datetime.now().time())[0:8]))
+    # try to send email
+    try:
+        if(use_ssl):
+            context = ssl.create_default_context()
+            with smtplib.SMTP_SSL(smtp_server_incoming, 465, context=context) as server:
+                server.login(sender, password)
+                server.sendmail(sender, receivers, message)
+        else:
+            connection = smtplib.SMTP(smtp_server_incoming, 587)
+            connection.ehlo()
+            connection.starttls()
+            connection.ehlo()
+            connection.login(sender, password)
+            connection.sendmail(sender, receivers, message)
+            connection.close()
 
+        logger.info("The mail has been sent")
+    except Exception as e:
+        logger.error("error while sending mail", exc_info=True)
+   
 
 
 
