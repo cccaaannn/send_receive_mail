a27ff3f12df25d370729bc29bfa84853ebec0213
can kurt <can.kurt.aa@gmail.com>
2020-02-17 09:05:06 +0300

    logging fixed

--- mail_functions.py
+++ mail_functions.py
@@ -26,26 +26,32 @@ def send_mail(sender, password, receivers, subject, body, body_html=None, Cc=[],
 
 
     # log stuff
-    logger = logging.getLogger(__name__)  
-    verbosity = {0:50,1:40,2:30,3:20}
-    if(verbosity.get(verbose)):
-        logger.setLevel(verbosity.get(verbose))
-    else:
-        logger.setLevel(20)
-    
-    # log formatter
-    formatter = logging.Formatter("%(asctime)s [%(levelname)s] -> %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+    logger = logging.getLogger("send_mail_logger")  
 
-    # file handler
-    if(log_file):
-        file_handler = logging.FileHandler(log_file)
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+    # .hasHandlers() is not working 
+    # print(logger.handlers)
+    # print(logger.hasHandlers())
+    # if logger exists don't add new handlers
+    if(not logger.handlers):
+        verbosity = {0:50,1:40,2:30,3:20}
+        if(verbosity.get(verbose)):
+            logger.setLevel(verbosity.get(verbose))
+        else:
+            logger.setLevel(20)
+        
+        # log formatter
+        formatter = logging.Formatter("[%(levelname)s] %(asctime)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
-    # stream handler
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
+        # file handler
+        if(log_file):
+            file_handler = logging.FileHandler(log_file)
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+
+        # stream handler
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
     
 
 
